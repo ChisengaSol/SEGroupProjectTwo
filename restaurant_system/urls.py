1fe70879bd0e7f34c323bd2c9999a6824a4abcd2
ChisengaSol <solmanzambia@gmail.com>
2020-04-14 08:18:47 +0000

    Third commit

--- restaurant_system/urls.py
+++ restaurant_system/urls.py
@@ -2,5 +2,6 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('', views.menuform)
+    path('', views.menuform),
+    path('order/', views.Orderform)
 ]
--- restaurant_system/views.py
+++ restaurant_system/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-
+from .models import Menu
 # Create your views here.
 def menuform(request):
-    return render(request,'restaurant_system/menu.html')
+    meals = Menu.objects.all()
+    return render(request,'restaurant_system/menu.html',{'meals': meals})
+
+def Orderform(request):
+    return render(request,'restaurant_system/order.html')
